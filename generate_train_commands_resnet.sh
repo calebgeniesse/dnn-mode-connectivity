4eaad991a04afc3ffb8ed065c302c04b1bcef934
calebgeniesse <calebgeniesse@gmail.com>
2023-08-07 22:08:16 -0700

    script for generating resnet experiments

---
+++ generate_train_commands_resnet.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+MODEL_1="/global/homes/g/geniesse/dnn-mode-connectivity/checkpoints/ResNet20_loss_lens/resnet20_batch_norm_True_residual_True_seed_SEED_net.pkl"
+
+SEEDS=(0 123 2023)
+
+for SEED_1 in "${SEEDS[@]}" ; do
+    for SEED_2 in "${SEEDS[@]}" ; do
+        
+        if [[ "${SEED_1}" == "${SEED_2}" ]] ; then
+            continue;
+        fi
+    
+        MODE_1="${MODEL_1/SEED/${SEED_1}}"
+        MODE_2="${MODEL_1/SEED/${SEED_2}}"
+
+        # echo ${MODE_1}
+        # echo ${MODE_2}
+        # echo ""
+
+        _cmd="python3 train_resnet.py \
+                --dir=/global/homes/g/geniesse/dnn-mode-connectivity/checkpoints \
+                --dataset=CIFAR10 \
+                --data_path=/global/homes/g/geniesse/data \
+                --transform=ResNet \
+                --model=ResNet20BNres \
+                --epochs=10 \
+                --lr=0.1 \
+                --wd=5e-4 \
+                --curve=Bezier \
+                --num_bends=3 \
+                --init_start=${MODE_1} \
+                --init_end=${MODE_2} \
+                --fix_start \
+                --fix_end \
+                --use_test"
+
+        echo ""
+        echo $_cmd
+        # eval $_cmd
+        echo ""
+
+    done
+done
+
+		 
